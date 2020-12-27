496866c43dc1206534034e9f5602fe489b014bae
midyantard2 <jixuanli1230@yahoo.com>
2020-12-26 20:06:41 -0500

    Hook Testing

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';  // useState (state Hook) useEffect (lifeCycle Hook)  ---- Hook
 import {robots} from '../../Components/Card/robots';
 import CardList from "../../Components/CardList/CardList";
 import SearchBox from "../../Components/SearchBox/SearchBox";
@@ -9,20 +9,18 @@ import Scroll from "../../Components/Scroll/Scroll";
  *   created bt Jixuan Li
  *   2020-12-25-16:47
  */
-class App extends Component {
 
-    constructor() {
-        super();
-        // have state => smart component
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+function App () {
 
-    //called after render
-    //fetch the API content
-    componentDidMount() {
+    // ------State Hook ---------
+    // robots is the state, and setRobots is the function that changes this state, in useState, the default state.
+    // array destructure....
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
+    const [count, setCount] = useState(0);
+
+    //-------life cycle hook-------
+    useEffect(() => {
         const temp =   {
             id: 113,
             name: 'Steve Rroboto',
@@ -30,37 +28,35 @@ class App extends Component {
             email: 'Steve.Roboto@kssv.biz'
         }
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then( users => {
+                .then(response => response.json())
+                .then( users => setRobots(users.concat(temp)))
+        },
+        // Optional State, tells react: run this effect when XXX changes,
+        // if we give an empty list, it means, run this when nothing changes ==> ComponentDidMount
+        // aka just once after changes
+        []
+        )
 
-                this.setState({robots: users.concat(temp)})
-            })
+    const onSearchChange = (event) => {
+        // don't need to set the entire state, just set SearchField  OOOOOOYEEEAAA
+        setSearchField( event.target.value)
     }
 
-// binding free function writing
-    onSearchChange = (event) => {
-        this.setState({searchField : event.target.value})
-    }
-
-    render() {
-        const {searchField, robots} = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        })
-        return  !robots.length  ?
-            <h1>Loading...</h1> :
-            <div className='tc'>
-                <h1 className='f2-l'>ROBOFRIENDS</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundry>
-                        <CardList robots={filteredRobots}/>
-                    </ErrorBoundry>
-                </Scroll>
-            </div>
-
-
-    }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    return  !robots.length  ?
+        <h1>Loading...</h1> :
+        <div className='tc'>
+            <h1 className='f2-l'>ROBOFRIENDS</h1>
+            <button onClick={() => setCount(count+1)}>Click Me</button>
+            <SearchBox searchChange={onSearchChange}/>
+            <Scroll>
+                <ErrorBoundry>
+                    <CardList robots={filteredRobots}/>
+                </ErrorBoundry>
+            </Scroll>
+        </div>
 
 }
 
